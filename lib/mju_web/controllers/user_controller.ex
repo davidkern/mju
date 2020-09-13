ab6b1cf05a09e11dd98a0099ab3aa9938d62bdf7
David Kern <david@mju.io>
2020-09-12 17:43:28 -0700

    Add password to user registration

--- lib/mju_web/controllers/user_controller.ex
+++ lib/mju_web/controllers/user_controller.ex
@@ -5,7 +5,7 @@ defmodule MJUWeb.UserController do
   alias MJU.Accounts.User
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
+    case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "#{user.name} created")
@@ -27,7 +27,7 @@ defmodule MJUWeb.UserController do
   end
 
   def new(conn, _params) do
-    changeset = Accounts.change_user(%User{})
+    changeset = Accounts.change_registration(%User{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 end
